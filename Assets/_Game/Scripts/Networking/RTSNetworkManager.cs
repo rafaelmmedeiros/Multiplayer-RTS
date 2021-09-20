d9d4ee8e63432816efb08c6bf2498bb4ac011809
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-09-20 13:38:07 -0300

    Movement

---
+++ Assets/_Game/Scripts/Networking/RTSNetworkManager.cs
@@ -0,0 +1,12 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RTS.Networking
+{
+    public class RTSNetworkManager : NetworkManager
+    {
+
+    }
+}
