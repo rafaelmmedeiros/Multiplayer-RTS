6e0af89563a7907e1e6025e55d29b0410f205fc0
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-10-09 20:39:19 -0300

    GUI

--- Assets/_Game/Scripts/GUI/BuildingButton.cs
+++ Assets/_Game/Scripts/GUI/BuildingButton.cs
@@ -1,8 +1,6 @@
 using RTS.Buildings;
 using RTS.Configs;
 using RTS.Networking;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
