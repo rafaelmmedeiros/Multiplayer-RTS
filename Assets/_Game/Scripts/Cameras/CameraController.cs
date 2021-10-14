976eaaf0aba1b98dbf362439eb820965f8e7d00d
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-10-13 21:22:19 -0300

    Cameras

--- Assets/_Game/Scripts/Cameras/CameraController.cs
+++ Assets/_Game/Scripts/Cameras/CameraController.cs
@@ -1,6 +1,8 @@
 using Mirror;
 using RTS.Configs;
+using RTS.Inputs;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace RTS.Cameras
 {
@@ -14,5 +16,26 @@ namespace RTS.Cameras
         [SerializeField] private float screenBorderThickness = 10f;
         [SerializeField] private Vector2 screenXLimits = Vector2.zero;
         [SerializeField] private Vector2 screenYLimits = Vector2.zero;
+
+        private Vector2 previousInput;
+
+        private Controls controls;
+
+        public override void OnStartAuthority()
+        {
+            playerCameraTransform.gameObject.SetActive(true);
+
+            controls = new Controls();
+
+            controls.Player.MoveCamera.performed += SetPreviousInput;
+            controls.Player.MoveCamera.canceled += SetPreviousInput;
+
+            controls.Enable();
+        }
+
+        private void SetPreviousInput(InputAction.CallbackContext context)
+        {
+            previousInput = context.ReadValue<Vector2>();
+        }
     }
 }
