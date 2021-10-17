585ecb079b0c7f731a2820f8e805c2813dc3f57f
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-10-17 19:37:59 -0300

    Menu

---
+++ Assets/_Game/Scripts/Menus/LobbyMenu.cs
@@ -0,0 +1,43 @@
+using Mirror;
+using RTS.Networking;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RTS.Menus
+{
+    public class LobbyMenu : MonoBehaviour
+    {
+        [SerializeField] private GameObject lobbyUI = null;
+
+        private void Start()
+        {
+            RTSNetworkManager.ClientOnConnected += HandleClientConnected;
+        }
+
+        private void OnDestroy()
+        {
+            RTSNetworkManager.ClientOnConnected -= HandleClientConnected;
+
+        }
+
+        private void HandleClientConnected()
+        {
+            lobbyUI.SetActive(true);
+        }
+
+        public void LeaveLobby()
+        {
+            if (NetworkServer.active && NetworkClient.isConnected)
+            {
+                NetworkManager.singleton.StopHost();
+            }
+            else
+            {
+                NetworkManager.singleton.StopClient();
+
+                SceneManager.LoadScene(0);
+            }
+        }
+
+    }
+}
